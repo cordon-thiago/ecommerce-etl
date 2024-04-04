02624e3dd3ea585e6eb93bc89599228c6e66755a
FARFETCH\thiago.c.rodrigues <thiago.c.rodrigues@farfetch.com>
2024-04-04 09:36:21 +0100

    Add test cases

---
+++ app/tests/test_etl.py
@@ -0,0 +1,25 @@
+import pytest
+import pandas as pd
+import sys
+sys.path.insert(0, '../functions')
+from functions import etl_functions as etl
+
+@pytest.fixture
+def test_data():
+    data = {
+        "key_column": [1,1,2,3],
+        "value_column": ["A", "B", "C", None]
+    }
+    df = pd.DataFrame(data=data)
+    return df
+
+def test_always_passes():
+    assert True
+
+def test_remove_duplicity(test_data):
+    df_transformed = etl.remove_duplicity(test_data, "key_column")
+    assert len(df_transformed) == 3
+    
+def test_fill_missing(test_data):
+    df_transformed = etl.fill_missing(test_data, {"value_column": "N/A"})
+    assert df_transformed.isnull().sum().sum() == 0
